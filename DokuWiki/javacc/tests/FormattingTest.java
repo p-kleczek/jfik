7eeee90b6bbc541715d95545d2235ad4499dde72
pkleczek <p.l.kleczek@gmail.com>
2013-04-06 21:38:01 +0200

    Initial commit.

---
+++ DokuWiki/javacc/tests/FormattingTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import main.EG1;
+import main.ParseException;
+
+import org.junit.Test;
+
+public class FormattingTest {
+
+	private EG1 parser;
+	
+	@Test
+	public void testBold() throws ParseException {
+		String text = "<sub>subscript</sub>**a**";
+		parser = new EG1(stringToInputStream(text));
+		parser.start();
+	}
+
+	@Test
+	public void testBold2() throws ParseException {
+		String text = "<sub>subscript</sub>**^%a**";
+		parser = new EG1(stringToInputStream(text));
+		parser.start();
+	}
+
+	private static InputStream stringToInputStream(String s) {
+		return new ByteArrayInputStream(s.getBytes());
+	}
+}
---
+++ DokuWiki/javacc/tests/SimpleTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import java.io.FileReader;
+
+public class SimpleTest {
+	public static void main(String[] args) {
+		try {
+			String filePath = "";
+//			EG1 parser = new EG1(new FileReader(filePath));
+//			parser.initParser();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
